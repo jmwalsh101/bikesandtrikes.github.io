064804556c8884138a808b9087531a7f2486cbc1
x21189145 <x21189145@student.ncirl.ie>
2022-04-20 12:28:33 +0100

    Updated contact

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -10,8 +10,76 @@ document.addEventListener("click", function (e) {
   }
 });
 
-// contact form
+// contact form 2
+
 function validateform() {
+  var nameVal = document.getElementById("name").value;
+  var phoneVal= document.getElementById("phone").value;
+  var emailVal = document.getElementById("emailAddress").value;
+  var messageVal = document.getElementById("message").value;
+  //contact array
+  var retainVals=[nameVal, phoneVal, emailVal, messageVal];
+  console.log(retainVals);
+  //local storage of contact array
+  localStorage.setItem("contactdata", retainVals);
+ 
+
+  //rev sound effect onclick
+ // document.getElementById("rev").play();
+ // alert(messageVal);
+  //if all inputs are empty
+  if (nameVal == "") {
+    //Error Code here
+    alert("Please enter your name.");
+    document
+      .getElementById("name")
+      .insertAdjacentHTML(
+        "afterend",
+        "<h7>Error:Your name must be at least 2 characters long.</h7>"
+      );	
+  }
+  else if (phoneVal.length < 7 || nameVal.length > 11) {
+    // Error Code here
+    alert("Please enter a valid mobile number.");
+  //  document.contactForm.name.focus();
+    document
+      .getElementById("phoneError")
+      .insertAdjacentHTML(
+        "afterend",
+        "<h7>Error:Your mobile must be at least 10 digits long.</h7>"
+      );
+  }
+  else if (emailVal.length < 11 || emailVal.length > 30 || !emailVal.includes("@")) {
+    // Error Code here
+    alert("Please re-enter your email.");
+    document
+      .getElementById("emailError")
+      .insertAdjacentHTML(
+        "afterend",
+        "<h7>Error:Please reenter your email.</h7>"
+      );
+  }
+  else if (messageVal.length < 10 || messageVal.length > 150) {
+    // Error Code here
+    console.log();
+    alert("please enter valid message");
+    document
+      .getElementById("msgError")
+      .insertAdjacentHTML(
+        "afterend",
+        "<h7>Error:Please enter valid message.</h7>"
+      );
+  }
+  else{
+	document.getElementById("OutputName").innerHTML = nameVal;
+	document.getElementById("OutputeMail").innerHTML = emailVal;
+	document.getElementById("displayForm").style.display="none";
+	document.getElementById("second").style.display="block";
+  }	
+}
+
+// contact form
+/* function validateform() {
   var nameVal = document.getElementById("name").value;
   var phoneVal= document.getElementById("phone").value;
   var emailVal = document.getElementById("emailAddress").value;
@@ -54,7 +122,7 @@ function validateform() {
     alert (nameVal + "thank you we will be in contact shortly via"+  emailVal);
   }
   
-}
+} */
 
 // video pause/play
 var v1Playing = true; //video 1
