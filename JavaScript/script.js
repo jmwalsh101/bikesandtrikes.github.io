5a2d500ed085bbf66cb3251d345123894e13c971
Joseph Walsh <josephwalsh@arista.com>
2022-04-21 12:47:53 +0100

    Code clean-up

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,4 @@
-// gallery lightbox
+// Gallery Lightbox
 document.addEventListener("click", function (e) {
   if (e.target.classList.contains("gallery-item")) {
     const src = e.target.getAttribute("src");
@@ -10,8 +10,7 @@ document.addEventListener("click", function (e) {
   }
 });
 
-// contact form 2
-
+// Contact Form Array
 function validateform() {
   var nameVal = document.getElementById("name").value;
   var phoneVal = document.getElementById("phone").value;
@@ -25,11 +24,9 @@ function validateform() {
 
   //rev sound effect onclick
   document.getElementById("rev").play();
-  //alert(messageVal);
   //if all inputs are empty
   if (nameVal == "") {
     //Error Code here
-    //alert("Please enter your name.");
     document
       .getElementById("name")
       .insertAdjacentHTML(
@@ -38,7 +35,6 @@ function validateform() {
       );
   } else if (phoneVal.length < 7 || phoneVal.length > 11) {
     // Error Code here
-    // alert("Please enter a valid mobile number.");
     document
       .getElementById("phoneError")
       .insertAdjacentHTML(
@@ -51,7 +47,6 @@ function validateform() {
     !emailVal.includes("@")
   ) {
     // Error Code here
-    // alert("Please re-enter your email.");
     document
       .getElementById("emailError")
       .insertAdjacentHTML(
@@ -60,8 +55,6 @@ function validateform() {
       );
   } else if (messageVal.length < 10 || messageVal.length > 150) {
     // Error Code here
-    // console.log();
-    // alert("please enter valid message");
     document
       .getElementById("msgError")
       .insertAdjacentHTML(
@@ -76,53 +69,7 @@ function validateform() {
   }
 }
 
-// contact form
-/* function validateform() {
-  var nameVal = document.getElementById("name").value;
-  var phoneVal= document.getElementById("phone").value;
-  var emailVal = document.getElementById("emailAddress").value;
-  var messageVal = document.getElementById("message").value;
-  //contact array
-  var retainVals=[nameVal, phoneVal, emailVal, messageVal];
-  console.log(retainVals);
-  //local storage of contact array
-  localStorage.setItem("contactdata", retainVals);
-
-  //rev sound effect onclick
-  document.getElementById("rev").play();
-  alert(messageVal);
-  //if all inputs are empty
-  if (nameVal == "" || phoneVal ==""|| emailVal == "" || messageVal == "") {
-  //Error Code here
-  alert("Please enter valid data.");
-  }
-  //phone validation
-  if (phoneVal.length < 7 || phoneVal.length > 11) {
-    // Error Code here
-    alert("Please enter a valid mobile number.");
-    //document.contactForm.name.focus();
-    document.getElementById("phoneError").insertAdjacentHTML("afterend","<h7>Error:Your mobile must be at least 10digits long.</h7>");
-  }
-  //email validation
-  if (emailVal.length < 11 || emailVal.length > 30 || !emailVal.includes("@")) {
-    // Error Code here
-    alert("Please re-enter your email.");
-    document.getElementById("emailError").insertAdjacentHTML("afterend","<h7>Error:Please reenter your email.</h7>");
-  }
-  //message validation
-  if (messageVal.length < 10 || messageVal.length > 150) {
-    // Error Code here
-    console.log();
-    alert("please enter valid message");
-    document.getElementById("msgError").insertAdjacentHTML("afterend","<h7>Error:Please enter valid message.</h7>");
-  }
- 
-    alert (nameVal + "thank you we will be in contact shortly via"+  emailVal);
-  }
-  
-} */
-
-// video pause/play
+// About Us video pause/play
 var v1Playing = true; //video 1
 var v2Playing = true; //video 2
 
